614bccf04b9efb4092f4400a886a34d3ebcfd177
Claudiu Morogan <claudiumorogan@gmail.com>
2024-01-09 16:21:41 +0200

    added new view

---
+++ sql/tables/views/ytb_songs_list.sql
@@ -0,0 +1,1 @@
+CREATE OR REPLACE VIEW `ytb_songs_list` AS select `yd`.`video_id` AS `video_id`,`ysd`.`artist` AS `artist`,`ysd`.`song` AS `song`,`yd`.`link` AS `link`,if(`yd`.`downloaded` = 1,'yes','no') AS `downloaded` from (`ytb_downloads` `yd` join `ytb_song_details` `ysd`) where `yd`.`video_id` = `ysd`.`video_id`
